7e9bda67c94da8a4c5bbc6a6070c7d7e2c198a79
Nhlakanipho <nhlaka.devv@gmail.com>
2022-01-23 02:55:06 +0200

    error handling on auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -56,8 +56,9 @@ export async function signUp(req: Request, res: Response){
 
     const duplicates = users.filter(targetUser => targetUser.email === email);
 
-    if (duplicates.length){
-        res.send("Email already registered");
+    if (typeof password !== "string" || duplicates.length){
+        const error: string = typeof password !== "string" ? "Password should be a string" : "Email already registered";
+        res.send(error);
         return;
     }
 
